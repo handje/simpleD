f3bd1e69a06ee0c390cafe42360de55cd47ca66d
Sonjieun <57988006+handje@users.noreply.github.com>
2022-05-04 13:35:45 +0900

    Update Login.js

--- simda/src/components/Login.js
+++ simda/src/components/Login.js
@@ -1,47 +1,132 @@
-import './Login.css';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import {Input, Button } from 'antd';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Form, Input, Button, Checkbox } from "antd";
+import "antd/dist/antd.min.css";
 
+function Login({ user, inputID, inputPWD, setInputID, setInputPWD, setUser }) {
+  function handleInputID(event) {
+    setInputID(event.target.value);
+  }
 
+  function handleInputPWD(event) {
+    setInputPWD(event.target.value);
+  }
 
-function Login({user,InputID,InputPWD,setInputID,setInputPWD,setUser}){
-    function axiosPost(LoginUser) {
-        axios.post("http://14.35.100.207:3000/users/login", LoginUser)
-            .then((response) => {
-                console.dir(response);
-             })
-            .catch((error)=>{console.log(error)})
-      }
+  const onFinish = (values) => {
+    console.log("Success:", values);
+    //event.preventDefault();
+    // if (!emailCheck(inputID)) {
+    //   window.alert("이메일 형식이 맞지 않습니다.");
+    //   return;
+    // }
+    const LoginUser = { user_id: inputID, user_password: inputPWD };
+    setUser(LoginUser);
+    setInputID("");
+    setInputPWD("");
+    axiosPost(LoginUser);
+  };
 
-    function handleInputID(event){
-        setInputID(event.target.value);
-    }
-    function handleInputPWD(event){
-        setInputPWD(event.target.value);
-    }
-    function handleSubmitLogin(event){
-        event.preventDefault();
-        console.log("click")
-        const LoginUser={user_id:InputID,user_password:InputPWD};
-        setUser(LoginUser)
-        setInputID("")
-        setInputPWD("")
-        axiosPost(LoginUser);
-    }
-    return(
-        <div className="Login">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmitLogin}>
-                <Input type="text" className="ID" required placeholder="ID" onChange={handleInputID} value={InputID}></Input>
-                <Input type="password" className="PWD" required placeholder="Password" onChange={handleInputPWD} value={InputPWD}></Input>
-                <Button shape="circle" className="submitLogin">Login</Button>
-            </form>
-            <Link to="/register"><Button type="dashed">Dashed Button</Button></Link>
-            <Link to="/register"><Button>Google</Button></Link>
-        </div>
-        
-    );
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  function emailCheck(id) {
+    return id.indexOf("@") !== -1;
+  }
+
+  function axiosPost(LoginUser) {
+    axios
+      .post("http://14.35.100.207:3000/users/login", {
+        user_id: inputID,
+        user_password: inputPWD,
+      })
+      .then((response) => {
+        console.dir(response);
+      })
+      .catch((error) => {
+        console.log("error");
+        console.log(error);
+      });
+  }
+
+  return (
+    <div className="Login">
+      <h1>Login</h1>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 8,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="Email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Email!",
+            },
+          ]}
+        >
+          <Input
+            onChange={handleInputID}
+            value={inputID}
+            placeholder="Please input your Email!"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password
+            onChange={handleInputPWD}
+            value={inputPWD}
+            placeholder="Please input your password!"
+          />
+        </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Link to="/register">
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Link>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default Login;
